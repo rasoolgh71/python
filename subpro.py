d58b0c600272e408351c78ad763a8ed5b1380339
rasoolgh71 <rasoolghanna@gmail.com>
2017-11-21 13:40:51 +0330

    subprocess size of disk

--- subpro.py
+++ subpro.py
@@ -1,17 +1,30 @@
 import os
 import sys
 import subprocess
+from pprint import pprint
 
-def main():
+def main():   #print sunprocess
     print("subprocees")
-def subp():
+#**********************************************************************************
+def subp():   #subprocess list program
     print("subprocess is ")
-    out= subprocess.call('ls')
-    print('out is:',out)
-    out1=subprocess.Popen(['ls','-la'])
-    print('ou is:',out1)
+    out= subprocess.call(['ls'])
 
+    out1=subprocess.Popen(['ls', '-la'])
+   # print(subprocess.check_output("df -h".split()).split("\n"))
+
+
+#**********************************************************************************
+
+def sdisk():  #subprocces list size of disk
+    diskinfo = subprocess.Popen('df -h', shell=True, stdout=subprocess.PIPE)
+    output = diskinfo.communicate()[0]
+    #for x in output[1:]:
+        #if int(x.split()[-2][:-1]) >= 10:
+           # print(x)
+    print(output)
 
 if __name__=="__main__":
     #main()
     subp()
+    sdisk()
